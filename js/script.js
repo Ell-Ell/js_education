b8c40343cdd48738a4495392b46048c714a0865b
Ell-Ell <ellell08061990@gmail.com>
2021-05-16 18:08:41 +0300

    b

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Переменная num может принимать 4 значения: 1, 2, 3 или 4. Если она имеет 
+// значение '1', то в переменную result запишем 'зима', если имеет значение 
+// '2' – 'весна' и так далее. Решите задачу через switch-case.
+
+// const num = prompt("введи число");
+// let result;
+
+// switch (+num) {
+//     case 1:
+//         result = "зима";
+//         break;
+//      case 2:
+//         result = "весна";
+//         break;
+//      case 3:
+//         result = "осень";
+//         break;
+//      case 4:
+//         result = "лето";
+//         break;
+//     default: result = "хз"    
+// }
+// console.log(result);
