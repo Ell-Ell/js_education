0b13ae9394c546b24c8d6413914d2509b70d1623
Ell-Ell <AllaAkhtyrskaya@gmail.com>
2021-06-22 23:13:24 +0300

    start

--- js/script.js
+++ js/script.js
@@ -1,23 +1,41 @@
-Переменная num может принимать 4 значения: 1, 2, 3 или 4. Если она имеет 
-значение '1', то в переменную result запишем 'зима', если имеет значение 
-'2' – 'весна' и так далее. Решите задачу через switch-case.
+// const types =
+// str = 'hello';
+// num = 12345;
+// symb = Symbol('hello') примитивный тип данных, нужен для создания уникальных идентификаторов;
+// bool = true;
+// bigInteger = BigInt(37827327382) для вычислений больше,чем 2 в 53 степени, криптовалюта;
+// arr = [];
+// obj = {};
+// und = undefined;
+// nu = null;
 
-const num = prompt("введи число");
-let result;
+// Напиши скрипт который просит пользователя ввести число в prompt
+// до тех пор, пока посетитель не нажмёт Cancel  и каждый раз
+// добавляет введенное число к общей сумме.
 
-switch (+num) {
-    case 1:
-        result = "зима";
-        break;
-     case 2:
-        result = "весна";
-        break;
-     case 3:
-        result = "осень";
-        break;
-     case 4:
-        result = "лето";
-        break;
-    default: result = "хз"    
-}
-console.log(result);
+// let total = 0;
+
+// while (true) {
+//     const enterSth = prompt('Enter a number');
+//     let result;
+//     if (+enterSth === null) {
+//         result = `total sum is ${total}`;
+//         break;
+//     }
+//     else if (!isNaN(enterSth)) {
+//         result = `total sum is ${total += +enterSth}`;
+//     }
+//     else {
+//         result = 'enter a number'
+//     }
+// }
+
+// or
+// while (true) {
+//     const a = prompt('Enter a number');
+//     if (a = null) {
+//         alert(`total = ${total}`);
+//         break;
+//     }
+//     !isNaN(a) ? (total += +a) : alert('Its not a number');
+// }
