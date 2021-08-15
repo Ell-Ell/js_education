60a12b689b1845ff66aa57f42dd9c5c55db0fc92
Ell-Ell <AllaAkhtyrskaya@gmail.com>
2021-08-15 16:38:38 +0300

    start

---
+++ js/debounce.js
@@ -0,0 +1,17 @@
+const f = () => console.log(1);
+
+let timeout = setTimeout(f, 100);
+const debounceTest = function (timeout) {
+  return;
+};
+
+debounceTest();
+debounceTest();
+debounceTest();
+// const f = () => console.log(1);
+
+// const _f = deb(f, 100);
+
+// _f();
+// _f();
+// _f();
